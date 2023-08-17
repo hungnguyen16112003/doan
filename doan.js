abd8bd400b3ad968c0e32966810bfb061cde7564
hungnguyen16112003 <nguyenquochung16112003@gmail.com>
2023-08-18 03:58:16 +0700

    project-doan

---
+++ doan.js
@@ -0,0 +1,37 @@
+const next = document.querySelector('.next')
+const prev = document.querySelector('.prev')
+const comment = document.querySelector('#list-comment')
+const commentItem = document.querySelectorAll('#list-comment .item')
+var translateY = 0
+var count = commentItem.length
+
+next.addEventListener('click', function (event) {
+  event.preventDefault()
+  if (count == 1) {
+    // Xem hết bình luận
+    return false
+  }
+  translateY += -400
+  comment.style.transform = `translateY(${translateY}px)`
+  count--
+})
+
+prev.addEventListener('click', function (event) {
+  event.preventDefault()
+  if (count == 3) {
+    // Xem hết bình luận
+    return false
+  }
+  translateY += 400
+  comment.style.transform = `translateY(${translateY}px)`
+  count++
+})
+var btn = document.getElementById("actions");
+var close = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+  modal.style.display = "block";
+}
+close.onclick = function () {
+  modal.style.display = "none";
+}
